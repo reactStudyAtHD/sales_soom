680e3a955d978faa68d01d21dca7d7f28448445f
Soom <developer.soom@gmail.com>
2020-04-10 20:55:38 +0900

    add

--- src/components/TableEl.js
+++ src/components/TableEl.js
@@ -1,39 +1,29 @@
 import React from 'react';
-import Table from 'react-bootstrap/Table';
 
+// const TableEl = (key, id, date, table, card, cash, service, total, per) => {
+// 	return (
+// 		<tr>
+// 			<td>{date}</td>
+// 			<td>{table}</td>
+// 			<td>{card}</td>
+// 			<td>{cash}</td>
+// 			<td>{service}</td>
+// 			<td>{per}</td>
+// 		</tr>
+//
+// 	);
+// };
 
-const TableElement = () => {
+const TableEl = (data) => {
+	console.log(data)
 	return (
-		<Table striped bordered hover>
-			<thead>
-			<tr>
-				<th>#</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-				<th>Username</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr>
-				<td>1</td>
-				<td>Mark</td>
-				<td>Otto</td>
-				<td>@mdo</td>
-			</tr>
-			<tr>
-				<td>2</td>
-				<td>Jacob</td>
-				<td>Thornton</td>
-				<td>@fat</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td colSpan="2">Larry the Bird</td>
-				<td>@twitter</td>
-			</tr>
-			</tbody>
-		</Table>
+		<tr>
+			<td>{data.SALE_DATE}</td>
+
+		</tr>
+	
 	);
 };
 
-export default TableElement;
+
+export default TableEl;
